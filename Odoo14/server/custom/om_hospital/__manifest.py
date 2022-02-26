d6da47b283c7e594c180e762d466913047b86a8b
HanZawNyine <hanzawnyine143@gmail.com>
2022-02-26 12:35:53 +0630

    create a module

---
+++ Odoo14/server/custom/om_hospital/__manifest.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+var = {
+    'name': 'Hospital Management',
+    'version': '14.0.1.0.0',
+    'summary': 'Hospital Management Software',
+    'sequence': 10,
+    'description': """Hospital Management Software""",
+    'category': 'Productivity',
+    'website': 'https://apps.odoo.com/',
+    'images': ['images/accounts.jpeg', 'images/bank_statement.jpeg', 'images/cash_register.jpeg',
+               'images/chart_of_accounts.jpeg', 'images/customer_invoice.jpeg', 'images/journal_entries.jpeg'],
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
