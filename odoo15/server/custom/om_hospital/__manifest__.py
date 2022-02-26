ece7606be5e0e377c3fa034a7dc9d6ae578bffdc
HanZawNyine <hanzawnyine143@gmail.com>
2022-02-26 15:51:52 +0630

    create module odoo15

---
+++ odoo15/server/custom/om_hospital/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Hospital Management',
+    'version': '1.0',
+    'summary': 'Hospital Management Software',
+    'sequence': -100,
+    'description': """Hospital Management Software""",
+    'category': 'Productivity',
+    'website': 'https://www.hanzawnyine.com',
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
