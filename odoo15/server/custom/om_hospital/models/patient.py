f1f7cec31af43f37e9070f657dea8da49ff75cec
HanZawNyine <hanzawnyine143@gmail.com>
2022-03-18 11:02:39 +0630

    Override Create Method

--- odoo15/server/custom/om_hospital/models/patient.py
+++ odoo15/server/custom/om_hospital/models/patient.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import api, fields, models, _, tools
+from odoo import api, fields, models, _, tools,api
 
 
 class HospitalPatient(models.Model):
@@ -7,16 +7,18 @@ class HospitalPatient(models.Model):
     _inherit = ["mail.thread", "mail.activity.mixin"]
     _description = "Hospital Patient"
 
-    name = fields.Char(string='Name', required=True, translate=True)
-    age = fields.Char(string='Age', required=True, translate=True)
+    name = fields.Char(string='Name', required=True, translate=True, tracking=True)
+    age = fields.Char(string='Age', required=True, translate=True, tracking=True)
     gender = fields.Selection([
         ('male', 'Male'),
         ('female', 'Female'),
         ('other', 'Other'),
-    ], required=True, default='other')
-    note = fields.Text(string='Description')
+    ], required=True, default='other', tracking=True)
+    note = fields.Text(string='Description', tracking=True)
     state = fields.Selection([('draft', 'Draft'), ('confirm', 'Confirmed'),
-                              ('done', 'Done'), ('cancel', 'Cancel')], default='draft', string="Status")
+                              ('done', 'Done'), ('cancel', 'Cancel')],
+                             default='draft', string="Status", tracking=True)
+    responsible_id = fields.Many2one(comodel_name='res.partner', string='Responsible')
 
     def action_confirm(self):
         self.state = 'confirm'
@@ -29,3 +31,11 @@ class HospitalPatient(models.Model):
 
     def action_cancel(self):
         self.state = 'cancel'
+
+    @api.model
+    def create(self, vals):
+        # print("success override create method")
+        if not vals.get("note"):
+            vals["note"] = 'New Patient'
+        res = super(HospitalPatient,self).create(vals)
+        return res
